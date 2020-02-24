63d219973d0a7b4a01cedcf03fc1001a635c29d7
skhkh19u292 <khamit-7@yandex.ru>
2020-02-24 19:29:03 +0300

    lab_01_0_2 added

---
+++ lab_01_0_2/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+	setbuf(stdout, NULL);
+	float a, b, h, side_edge, perimetr;
+	printf("Input trapezoid bases a, b and height h: ");
+	scanf("%f %f %f", &a, &b, &h);
+	side_edge = sqrt(pow((a-b)/2, 2) + pow(h, 2));
+	perimetr = side_edge*2 + a + b;
+	printf("%.5f", perimetr);
+	return 0;
+}
