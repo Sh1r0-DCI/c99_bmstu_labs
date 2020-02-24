ef3347242ba6aaf534180c9b6a4f13b612f79863
skhkh19u292 <khamit-7@yandex.ru>
2020-02-24 22:06:53 +0300

    lab_01_0_4 with text.txt was added

---
+++ lab_01_0_4/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+	setbuf(stdout, NULL);
+	int s, num_of_bottles, all_bottles = 0;
+	printf("Input amount of money s: ");
+	scanf("%d", &s);
+	while(s >= 45)
+	{
+		num_of_bottles = s / 45;
+		s %= 45;
+		s += num_of_bottles * 20;
+		all_bottles += num_of_bottles;
+	}
+	printf("%d", all_bottles);
+	return 0;
+}
