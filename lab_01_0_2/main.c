d9804be8481afcb67aa0881784a3126e5845facd
skhkh19u292 <khamit-7@yandex.ru>
2020-02-24 19:40:51 +0300

    lab_01_0_2 fixed

--- lab_01_0_2/main.c
+++ lab_01_0_2/main.c
@@ -7,8 +7,8 @@ int main(void)
 	float a, b, h, side_edge, perimetr;
 	printf("Input trapezoid bases a, b and height h: ");
 	scanf("%f %f %f", &a, &b, &h);
-	side_edge = sqrt(pow((a-b)/2, 2) + pow(h, 2));
-	perimetr = side_edge*2 + a + b;
+	side_edge = sqrt(pow((a - b)/2, 2) + pow(h, 2));
+	perimetr = side_edge * 2 + a + b;
 	printf("%.5f", perimetr);
 	return 0;
 }
