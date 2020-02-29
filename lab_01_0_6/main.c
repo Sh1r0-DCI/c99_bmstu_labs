ef3fd78d8ce2ee675db25c2aabbee103ee2dac25
skhkh19u292 <khamit-7@yandex.ru>
2020-02-29 14:27:44 +0300

    lab_01_0_6 fixed.

--- lab_01_0_6/main.c
+++ lab_01_0_6/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 
-int angle_check(float max, float min1, float min2)
+int angle_check(double max, double min1, double min2)
 {
+	if (fabs(max - min1 - min2) < 0.0001)
+	{
+		return -1;
+	}
 	if (pow(max, 2) < pow(min1, 2) + pow(min2, 2))
 	{
 		return 0;
@@ -17,7 +21,7 @@ int angle_check(float max, float min1, float min2)
 	}
 }
 
-int type_of_triangle(float x1, float y1, float x2, float y2, float x3, float y3)
+int type_of_triangle(double x1, double y1, double x2, double y2, double x3, double y3)
 {
 	float l1, l2, l3;
 	l1 = sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2));
@@ -27,7 +31,7 @@ int type_of_triangle(float x1, float y1, float x2, float y2, float x3, float y3)
 	{
 		if (l1 >= l3)
 		{
-		    return angle_check(l1, l2, l3);
+			return angle_check(l1, l2, l3);
 		}
 		else
 		{
@@ -45,21 +49,26 @@ int type_of_triangle(float x1, float y1, float x2, float y2, float x3, float y3)
 			return angle_check(l3, l1, l2);
 		}
 	}
-	
 }
 
 int main(void)
 {
 	setbuf(stdout, NULL);
-	int rc;
-	float x1, y1, x2, y2, x3, y3;
+	int rc, ans;
+	double x1, y1, x2, y2, x3, y3;
 	printf("Input coordinates of the triangle: ");
-	rc = scanf("%f %f %f %f %f %f", &x1, &y1, &x2, &y2, &x3, &y3);
+	rc = scanf("%lf %lf %lf %lf %lf %lf", &x1, &y1, &x2, &y2, &x3, &y3);
 	if (rc != 6)
 	{
 		printf("Input error.");
 		return 1;
 	}
-	printf("%d",type_of_triangle(x1, y1, x2, y2, x3, y3));
+	ans = type_of_triangle(x1, y1, x2, y2, x3, y3);
+	if (ans == -1)
+	{
+		printf("Incorrect coordinates for triangle.");
+		return 2;
+	}
+	printf("%d", ans);
 	return 0;
 }
