f43f4304423e22465604eff755eec5fa53da6ba2
Salatov Khamit <835-skhkh19u292@users.noreply.git.iu7.bmstu.ru>
2020-05-04 16:50:28 +0300

    Update main.c

--- lab_01_0_5/main.c
+++ lab_01_0_5/main.c
@@ -6,31 +6,31 @@
 
 int power(int a, int n)
 {
-	int temp = 1;
-	for (int i = 0; i < n; i++)
-	{
-		temp *= a;
-	}
-	return temp;
+    int temp = 1;
+    for (int i = 0; i < n; i++)
+    {
+        temp *= a;
+    }
+    return temp;
 }
 
 int main(void)
 {
-	setbuf(stdout, NULL);
-	int rc, a, n, ans;
-	printf("Input integer a and natural (non-negative integer) n: ");
-	rc = scanf("%d %d", &a, &n);
-	if (rc != 2)
-	{
-		printf("Input error.");
-		return INPUT_ERROR;
-	}
-	if (n < 1)
-	{
-		printf("Input error. n should be non-negative integer.");
-		return WRONG_INPUT_OF_N;
-	}
-	ans = power(a, n);
-	printf("%d", ans);
-	return OK;
+    setbuf(stdout, NULL);
+    int rc, a, n, ans;
+    printf("Input integer a and natural (non-negative integer) n: ");
+    rc = scanf("%d %d", &a, &n);
+    if (rc != 2)
+    {
+        printf("Input error.");
+        return INPUT_ERROR;
+    }
+    if (n < 1)
+    {
+        printf("Input error. n should be non-negative integer.");
+        return WRONG_INPUT_OF_N;
+    }
+    ans = power(a, n);
+    printf("%d", ans);
+    return OK;
 }
