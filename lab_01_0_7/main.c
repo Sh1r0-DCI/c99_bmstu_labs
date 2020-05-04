9c9b82049fc92edfd28273c4e21f729093ba1dce
Salatov Khamit <835-skhkh19u292@users.noreply.git.iu7.bmstu.ru>
2020-05-04 16:54:00 +0300

    Update main.c

--- lab_01_0_7/main.c
+++ lab_01_0_7/main.c
@@ -7,40 +7,40 @@
 
 double function_f(double x)
 {
-	return exp(x);
+    return exp(x);
 }
 
 double function_s(double x, double eps)
 {
-	double ans = 0, el = 1;
-	for (int i = 0; fabs(el) > eps; i++)
-	{
-		ans += el;
-		el = el * x / (i + 1);
-	}
-	return ans;
+    double ans = 0, el = 1;
+    for (int i = 0; fabs(el) > eps; i++)
+    {
+        ans += el;
+        el = el * x / (i + 1);
+    }
+    return ans;
 }
 
 int main(void)
 {
-	setbuf(stdout, NULL);
-	int rc;
-	double x, eps;
-	printf("Input x and eps: ");
-	rc = scanf("%lf %lf", &x, &eps);
-	if (rc != 2)
-	{
-		printf("Input error.");
-		return INPUT_ERROR;
-	}
-	if (eps <= 0 || eps >= 1)
-	{
-		printf("Incorrect input of eps.");
-		return EPS_INPUT_ERROR;
-	}
-	printf("s(x) = %lf\nf(x) = %lf\n", function_s(x, eps), function_f(x));
-	printf("absolute error: %lf\nrelative error: %lf", \
-	fabs(function_f(x) - function_s(x, eps)), \
-	fabs((function_f(x) - function_s(x, eps)) / function_f(x)));	
-	return OK;
+    setbuf(stdout, NULL);
+    int rc;
+    double x, eps;
+    printf("Input x and eps: ");
+    rc = scanf("%lf %lf", &x, &eps);
+    if (rc != 2)
+    {
+        printf("Input error.");
+        return INPUT_ERROR;
+    }
+    if (eps <= 0 || eps >= 1)
+    {
+        printf("Incorrect input of eps.");
+        return EPS_INPUT_ERROR;
+    }
+    printf("s(x) = %lf\nf(x) = %lf\n", function_s(x, eps), function_f(x));
+    printf("absolute error: %lf\nrelative error: %lf", \
+    fabs(function_f(x) - function_s(x, eps)), \
+    fabs((function_f(x) - function_s(x, eps)) / function_f(x)));	
+    return OK;
 }
