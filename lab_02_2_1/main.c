134fdb7cc0389637590196dc5d812c2211a8d7b6
skhkh19u292 <khamit-7@yandex.ru>
2020-06-14 19:32:58 +0300

    tabs fixed.

--- lab_02_2_1/main.c
+++ lab_02_2_1/main.c
@@ -9,12 +9,10 @@
 int negative_els(int arr[], int n)
 {
 	int ne = 0;
-    for (int i = 0; i < n; i++) 
-    {
-	    if (arr[i] < 0)
-		{
-		    ne += 1;
-		}
+	for (int i = 0; i < n; i++) 
+	{
+		if (arr[i] < 0)
+			ne += 1;
     }
 	return ne;
 }
@@ -27,26 +25,26 @@ int input_array(int arr[], int n)
     {
 	    printf("arr[%d] = ", i);
 	    rc = scanf("%d", &arr[i]);
-	    if (rc != 1)
-	        return INPUT_ERROR;
-    }
+		if (rc != 1)
+			return INPUT_ERROR;
+	}
 	return negative_els(arr, n);
 }
 
 void output_array(int arr[], int n)
 {
 	printf("array: ");
-    for (int i = 0; i < n; i++)
-	    printf("%d ", arr[i]);
+	for (int i = 0; i < n; i++)
+		printf("%d ", arr[i]);
 }
 
 double average_negatives(int arr[], int n, int ne)
 {
     double an;
 	for (int i = 0; i < n; i++)
-	    if (arr[i] < 0)
+		if (arr[i] < 0)
 		{
-		    an += arr[i];
+			an += arr[i];
 		}
 	an /= ne;
 	return an;
@@ -54,23 +52,23 @@ double average_negatives(int arr[], int n, int ne)
 
 int main(void)
 {
-    setbuf(stdout, NULL);
-    int rc;
+	setbuf(stdout, NULL);
+	int rc;
 	int n, ne;
-    printf("Input n - size of the array: ");
-    rc = scanf("%d", &n);
-    if (rc != 1)
-    {
-        printf("Input error.");
-        return INPUT_ERROR;
-    }
+	printf("Input n - size of the array: ");
+	rc = scanf("%d", &n);
+	if (rc != 1)
+	{
+		printf("Input error.");
+		return INPUT_ERROR;
+	}
 	int arr[N];
 	ne = input_array(arr, n);
 	if (ne == INPUT_ERROR)
-    {
-        printf("Input error.");
-        return INPUT_ERROR;
-    }
+	{
+		printf("Input error.");
+		return INPUT_ERROR;
+	}
 	if (ne == 0)
 	{
 		printf("No negative elements in array.");
@@ -78,5 +76,5 @@ int main(void)
 	}
 	printf("average of negatives is %lf\n", average_negatives(arr, n, ne));
 	output_array(arr, n);
-    return OK;
+	return OK;
 }
