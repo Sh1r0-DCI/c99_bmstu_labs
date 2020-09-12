ae14c832516300017e08760f022f0e05233a87b9
skhkh19u292 <khamit-7@yandex.ru>
2020-09-12 14:03:43 +0300

    fix in problem 05 in counting pair element

--- lab_02_0_5/funcs.c
+++ lab_02_0_5/funcs.c
@@ -3,11 +3,11 @@
 int max_sum(int *const pa_start, int *const pa_end)
 {
     const int *pa = pa_start;
+    const int *pa_backwards = pa_end;
     int sum = *pa_start + *(pa_end - 1);
     for (int i = 0; i <= (pa_end - pa_start - 1) / 2; i++, pa++)
     {
-        int *pa_backwards = pa_end - i - 1;
-        if (*pa + *pa_backwards > sum)
+        if (*pa + *pa_backwards-- > sum)
             sum = *pa + *pa_backwards;
     }
     return sum;
