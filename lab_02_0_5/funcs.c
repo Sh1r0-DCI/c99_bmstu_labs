60f5aa9163d4a3e540c08a102525109b87ff3841
skhkh19u292 <khamit-7@yandex.ru>
2020-09-12 14:11:55 +0300

    memory fix in problem 5

--- lab_02_0_5/funcs.c
+++ lab_02_0_5/funcs.c
@@ -3,11 +3,11 @@
 int max_sum(int *const pa_start, int *const pa_end)
 {
     const int *pa = pa_start;
-    const int *pa_backwards = pa_end;
-    int sum = *pa_start + *(pa_end - 1);
-    for (int i = 0; i <= (pa_end - pa_start - 1) / 2; i++, pa++)
+    const int *pa_backwards = pa_end - 1;
+    int sum = *pa_start + *pa_backwards;
+    for (int i = 0; i <= (pa_end - pa_start - 1) / 2; i++, pa++, pa_backwards--)
     {
-        if (*pa + *pa_backwards-- > sum)
+        if (*pa + *pa_backwards > sum)
             sum = *pa + *pa_backwards;
     }
     return sum;
