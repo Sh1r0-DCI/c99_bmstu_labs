31e17c72b0f05a6110ed95789ad3052e84c0056d
skhkh19u292 <khamit-7@yandex.ru>
2020-09-13 19:23:12 +0300

    M constant change in in_out.h

--- lab_03_0_3/in_out.h
+++ lab_03_0_3/in_out.h
@@ -1,9 +1,9 @@
 #ifndef _IN_OUT_H_
 
 #define _IN_OUT_H_
-#define M 10
+#define MAX_POSSIBLE_M 10
 
-int matrix_input(int matr[][M], int *pn, int *pm);
-void matrix_output(int matr[][M], int n, int m);
+int matrix_input(int matr[][MAX_POSSIBLE_M], int *pn, int *pm);
+void matrix_output(int matr[][MAX_POSSIBLE_M], int n, int m);
 
 #endif
