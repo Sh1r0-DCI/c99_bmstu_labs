2937df046d1766ab831b4d72f0b32e590fc7122b
skhkh19u292 <khamit-7@yandex.ru>
2020-12-24 00:58:08 +0300

    agrs check fix in sort

--- lab_07_03/src/main.c
+++ lab_07_03/src/main.c
@@ -27,14 +27,12 @@ int main(int argc, char *argv[])
     {
         return UNEXISTING_FILE;
     }
-    //printf("f_in open ok\n");
 
     if (fget_numbers(f_in, &numbers) == INPUT_ERROR)
     {
         fclose(f_in);
         return INPUT_ERROR;
     }
-    //printf("fget_numbers ok\n");
 
     if (numbers > 0)
     {
@@ -49,7 +47,6 @@ int main(int argc, char *argv[])
             free(in_array_begin);
             return INPUT_ERROR;
         }
-        //printf("read ok\n");
 
         f_out = fopen(argv[2], "w");
 
@@ -59,7 +56,6 @@ int main(int argc, char *argv[])
             fclose(f_in);
             return FOPEN_ERROR;
         }
-        //printf("both opens ok\n");
 
         if (argc == 4 && (*argv[3]) != 'f')
         {
@@ -78,7 +74,6 @@ int main(int argc, char *argv[])
             //printf("started sorting\n");
             mysort(out_array_begin, filtered, sizeof(int), compare_int_fn);
             //printf("sort ok\n");
-            //printf("key ok, starting printing out_array\n");
             print_to_file(f_out, out_array_begin, out_array_end);
             free(in_array_begin);
             free(out_array_begin);
@@ -89,9 +84,7 @@ int main(int argc, char *argv[])
             mysort(in_array_begin, numbers, sizeof(int), compare_int_fn);
             //printf("sort ok, starting printing in_array\n");
             print_to_file(f_out, in_array_begin, in_array_end);
-            //printf("print ok\n");
             free(in_array_begin);
-            //printf("free ok\n");
         }
     }
     else
--- lab_07_03/src/sort.c
+++ lab_07_03/src/sort.c
@@ -12,11 +12,16 @@ static void swap(int *d, int *s)
 
 void mysort(void *begin, size_t elements, size_t element_size, int (*compare_fn)(const void *, const void *))
 {
+    if (begin == NULL || elements == 0 || element_size == 0 || compare_fn == NULL)
+    {
+        return;
+    }
+
     int *end = (int *) ((char *) begin + (elements * element_size));
-    int *second_it = begin;
+
     for (int *main_it = begin; main_it < end - 1; main_it++)
     {
-        for (second_it = main_it + 1; second_it < end; second_it++)
+        for (int *second_it = main_it + 1; second_it < end; second_it++)
         {
             if ((*compare_fn)(main_it, second_it) > 0)
             {
